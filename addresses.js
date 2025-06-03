d1405ab7f21784883c4ac2153059771d1f8f7de2
ChristineEremija <christineeremija@solarpros.io>
2025-06-02 21:18:54 -0700

    addresses.js

---
+++ addresses.js
@@ -0,0 +1,44 @@
+const addresses = [
+    {
+        name: "DAUNTLESS",
+        poc: "Casey Mosier",
+        phone: "801-787-2621",
+        address: "15601 Garland Cir. Westminster CA 92683",
+        coords: [33.7436, -117.9677]
+    },
+    {
+        name: "MAMBAS",
+        poc: "Cayden Larsen",
+        phone: "970-966-0240",
+        address: "2525 NATOMAS PARK DR. 3RD FLOOR, SUITE 320. SACROMENTO, CA",
+        coords: [38.6313, -121.5051]
+    },
+    {
+        name: "UNITED",
+        poc: "Casey / Cam Kendall",
+        phone: "206-554-1489",
+        address: "340 S EAST END AVE. SUITE F. PAMONA, CA. 91766",
+        coords: [34.0551, -117.7497]
+    },
+    {
+        name: "THE SPARTANS",
+        poc: "Spencer deavila/ Zak Hattab",
+        phone: "484-401-0077",
+        address: "27141 Hideaway Ave. Santa Clarita, 91351",
+        coords: [34.4264, -118.4842]
+    },
+    {
+        name: "MOTION",
+        poc: "Junior Bandeira",
+        phone: "773-501-5285",
+        address: "6144 Industrial Way. Suite B. Livermore, CA. 94551",
+        coords: [37.7038, -121.7680]
+    },
+    {
+        name: "PRIME",
+        poc: "Hayden Young",
+        phone: "385-219-0979",
+        address: "3536 W Dorothea Ave. Visalia, CA. 93277",
+        coords: [36.2995, -119.3258]
+    }
+];
