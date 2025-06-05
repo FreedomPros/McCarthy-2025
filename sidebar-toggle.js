5d54aa1165e00a744f089b05aadd9c2b814df14a
ChristineEremija <christineeremija@solarpros.io>
2025-06-04 18:00:25 -0700

    sidebar-toggle.js

--- sidebar-toggle.js
+++ sidebar-toggle.js
@@ -1,11 +1,14 @@
 const sidebar = document.getElementById('sidebar');
 const toggleBtn = document.getElementById('sidebar-toggle');
+const mapDiv = document.getElementById('map');
 
 function updateArrow() {
     if (sidebar.classList.contains('sidebar-closed')) {
         toggleBtn.innerHTML = '&#x25B6;'; // ► right arrow, expand
+        mapDiv.classList.remove('map-with-sidebar');
     } else {
         toggleBtn.innerHTML = '&#x25C0;'; // ◄ left arrow, collapse
+        mapDiv.classList.add('map-with-sidebar');
     }
 }
 toggleBtn.addEventListener('click', function() {
