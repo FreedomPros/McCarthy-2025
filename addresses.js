c15e98a2f74534581fbac971122829505487906f
ChristineEremija <christineeremija@solarpros.io>
2025-06-09 14:40:16 -0700

    addresses.js

--- addresses.js
+++ addresses.js
@@ -1,1 +1,66 @@
-
+window.addresses = [
+  {
+    name: "FURY (150 - 200) CA",
+    poc: "Jay Merrifield",
+    phone: "801-336-6924",
+    address: "15440 Laguna Canyon Rd. Suite 210. Irvine, CA. 92618",
+    coords: [33.6644, -117.7576],
+    date: "June 26 - 27"
+  },
+  {
+    name: "MOMENTVS (70-80) CA",
+    poc: "Cole Sexton & Christian Harrison",
+    phone: "320-339-1204",
+    address: "10532 Acacia St. Rancho Cucomongo, Ca. 91730",
+    coords: [34.0921, -117.5737],
+    date: "June 23 - 24"
+  },
+  {
+    name: "DAUNTLESS & MOTION (60-70) CA",
+    poc: "Casey Mosier",
+    phone: "801-787-2621",
+    address: "15601 Garland Cir. Westminster CA 92683",
+    coords: [33.7436, -117.9677],
+    date: "June 30 - July 1"
+  },
+  {
+    name: "BAN, UNITED, & FORGE (65-75) CA",
+    poc: "Grant Misbach",
+    phone: "(801) 643-9206",
+    address: "10532 Acacia St. Rancho Cucomongo, Ca. 91730",
+    coords: [34.0921, -117.5743],
+    date: "July 3 - 4"
+  },
+  {
+    name: "THE SPARTANS & EMPIRE (15) CA",
+    poc: "Spencer deavila/ Zak Hattab",
+    phone: "484-401-0077",
+    address: "27141 Hideaway Ave. Santa Clarita, 91351",
+    coords: [34.4264, -118.4842],
+    date: "July 7"
+  },
+  {
+    name: "PRIME & APOLLO (30) CA",
+    poc: "Hayden Young",
+    phone: "385-219-0979",
+    address: "3536 W Dorothea Ave. Visalia, CA. 93277",
+    coords: [36.2995, -119.3258],
+    date: "July 9"
+  },
+  {
+    name: "MOTION (50+) CA",
+    poc: "Junior Bandeira",
+    phone: "773-501-5285",
+    address: "6144 Industrial Way. Suite B. Livermore, CA. 94551",
+    coords: [37.7038, -121.7680],
+    date: "July 11 - 12"
+  },
+  {
+    name: "PRIME, VITALITY, ENVISION (85-90) IL",
+    poc: "Chayse Kidd",
+    phone: "(208) 360-3024",
+    address: "4300 Commerce Ct, unit 240 Lisle, IL 60532",
+    coords: [41.8023, -88.0884],
+    date: "July 14 - 15"
+  }
+];
