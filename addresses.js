ec9e3d3b9283714c25963f2b0cbe0e4c74d827f7
ChristineEremija <christineeremija@solarpros.io>
2025-06-09 15:09:30 -0700

    Create addresses.js

---
+++ addresses.js
@@ -0,0 +1,10 @@
+window.addresses = [
+  {
+    name: "Test Marker (CA)",
+    poc: "Test Person",
+    phone: "123-456-7890",
+    address: "123 Main St, Los Angeles, CA",
+    coords: [34.0522, -118.2437],
+    date: "June 1 - 2"
+  }
+];
