d44fe5810e6e1265bb9597d62c9fe7f4baa51150
ChristineEremija <christineeremija@solarpros.io>
2025-06-09 15:13:24 -0700

    Update addresses.js

--- addresses.js
+++ addresses.js
@@ -1,10 +1,11 @@
 window.addresses = [
   {
-    name: "Test Marker (CA)",
-    poc: "Test Person",
-    phone: "123-456-7890",
-    address: "123 Main St, Los Angeles, CA",
-    coords: [34.0522, -118.2437],
-    date: "June 1 - 2"
+    name: "FURY (150 - 200) CA",
+    poc: "Jay Merrifield",
+    phone: "801-336-6924",
+    address: "15440 Laguna Canyon Rd. Suite 210. Irvine, CA. 92618",
+    coords: [33.6644, -117.7576],
+    date: "June 26 - 27"
   }
+  // ... more addresses ...
 ];
