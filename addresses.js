242dd38a02342423b3a572278c6d53e145a4b46f
ChristineEremija <christineeremija@solarpros.io>
2025-06-09 19:25:02 -0700

    addresses.js

--- addresses.js
+++ addresses.js
@@ -1,66 +1,75 @@
 window.addresses = [
   {
     name: "FURY (150 - 200) CA",
-    poc: "Jay Merrifield",
-    phone: "801-336-6924",
     address: "15440 Laguna Canyon Rd. Suite 210. Irvine, CA. 92618",
-    coords: [33.6644, -117.7576],
+    contacts: [
+      "Jay Merrifield (801-336-6924)"
+    ],
     date: "June 26 - 27"
   },
   {
     name: "MOMENTVS (70-80) CA",
-    poc: "Cole Sexton & Christian Harrison",
-    phone: "320-339-1204",
     address: "10532 Acacia St. Rancho Cucomongo, Ca. 91730",
-    coords: [34.0921, -117.5737],
+    contacts: [
+      "Cole Sexton (320-339-1204)",
+      "Christian Harrison (678) 702-7185"
+    ],
     date: "June 23 - 24"
   },
   {
     name: "DAUNTLESS & MOTION (60-70) CA",
-    poc: "Casey Mosier",
-    phone: "801-787-2621",
     address: "15601 Garland Cir. Westminster CA 92683",
-    coords: [33.7436, -117.9677],
+    contacts: [
+      "DAUNTLESS: Casey Mosier (801-787-2621)",
+      "MOTION: Jacob Garza (623) 308-9737",
+      "MOTION: Tilo Chen (510) 402-9699"
+    ],
     date: "June 30 - July 1"
   },
   {
     name: "BAN, UNITED, & FORGE (65-75) CA",
-    poc: "Grant Misbach",
-    phone: "(801) 643-9206",
     address: "10532 Acacia St. Rancho Cucomongo, Ca. 91730",
-    coords: [34.0921, -117.5743],
+    contacts: [
+      "BAN: Grant Misbach (801) 643-9206",
+      "UNITED: Casey Kendall (206) 554-1489",
+      "UNITED: Cameron Kendall (206) 661-4244",
+      "FORGE: Logan Berger (208) 954-1233"
+    ],
     date: "July 3 - 4"
   },
   {
     name: "THE SPARTANS & EMPIRE (15) CA",
-    poc: "Spencer deavila/ Zak Hattab",
-    phone: "484-401-0077",
     address: "27141 Hideaway Ave. Santa Clarita, 91351",
-    coords: [34.4264, -118.4842],
+    contacts: [
+      "THE SPARTANS: Spencer deavila/ Zak Hattab (484) 401-0077",
+      "EMPIRE: Sebastian Rodriguez (813) 679-0944"
+    ],
     date: "July 7"
   },
   {
     name: "PRIME & APOLLO (30) CA",
-    poc: "Hayden Young",
-    phone: "385-219-0979",
     address: "FreedomForever: 3010 San Antonio Dr. Fowler, CA 93625",
-    coords: [36.6177, -119.6782],
+    contacts: [
+      "PRIME: Hayden Young (385-219-0979)",
+      "APOLLO: Nate Girsberger (385) 210-7846"
+    ],
     date: "July 9"
   },
   {
     name: "MOTION (50+) CA",
-    poc: "Junior Bandeira",
-    phone: "773-501-5285",
     address: "6144 Industrial Way. Suite B. Livermore, CA. 94551",
-    coords: [37.7038, -121.7680],
+    contacts: [
+      "Junior Bandeira (773-501-5285)"
+    ],
     date: "July 11 - 12"
   },
   {
     name: "PRIME, VITALITY, ENVISION (85-90) IL",
-    poc: "Chayse Kidd",
-    phone: "(208) 360-3024",
     address: "4300 Commerce Ct, unit 240 Lisle, IL 60532",
-    coords: [41.8023, -88.0884],
+    contacts: [
+      "VITALITY: Chayse Kidd (208) 360-3024",
+      "ENVISION: Rocky Niger (727) 748-6089"
+    ],
     date: "July 14 - 15"
   }
 ];
