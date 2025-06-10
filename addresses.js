b9238c59fff14ab6f762e812f8ac198b919af5ce
ChristineEremija <christineeremija@solarpros.io>
2025-06-09 20:01:55 -0700

    addresses.js

--- addresses.js
+++ addresses.js
@@ -2,6 +2,7 @@ window.addresses = [
   {
     name: "FURY (150 - 200) CA",
     address: "15440 Laguna Canyon Rd. Suite 210. Irvine, CA. 92618",
+    coords: [33.6644, -117.7576],
     contacts: [
       "Jay Merrifield (801-336-6924)"
     ],
@@ -10,6 +11,7 @@ window.addresses = [
   {
     name: "MOMENTVS (70-80) CA",
     address: "10532 Acacia St. Rancho Cucomongo, Ca. 91730",
+    coords: [34.0921, -117.5737],
     contacts: [
       "Cole Sexton (320-339-1204)",
       "Christian Harrison (678) 702-7185"
@@ -19,6 +21,7 @@ window.addresses = [
   {
     name: "DAUNTLESS & MOTION (60-70) CA",
     address: "15601 Garland Cir. Westminster CA 92683",
+    coords: [33.7436, -117.9677],
     contacts: [
       "DAUNTLESS: Casey Mosier (801-787-2621)",
       "MOTION: Jacob Garza (623) 308-9737",
@@ -29,6 +32,7 @@ window.addresses = [
   {
     name: "BAN, UNITED, & FORGE (65-75) CA",
     address: "10532 Acacia St. Rancho Cucomongo, Ca. 91730",
+    coords: [34.0921, -117.5743],
     contacts: [
       "BAN: Grant Misbach (801) 643-9206",
       "UNITED: Casey Kendall (206) 554-1489",
@@ -40,6 +44,7 @@ window.addresses = [
   {
     name: "THE SPARTANS & EMPIRE (15) CA",
     address: "27141 Hideaway Ave. Santa Clarita, 91351",
+    coords: [34.4264, -118.4842],
     contacts: [
       "THE SPARTANS: Spencer deavila/ Zak Hattab (484) 401-0077",
       "EMPIRE: Sebastian Rodriguez (813) 679-0944"
@@ -49,6 +54,7 @@ window.addresses = [
   {
     name: "PRIME & APOLLO (30) CA",
     address: "FreedomForever: 3010 San Antonio Dr. Fowler, CA 93625",
+    coords: [36.6177, -119.6782],
     contacts: [
       "PRIME: Hayden Young (385-219-0979)",
       "APOLLO: Nate Girsberger (385) 210-7846"
@@ -58,6 +64,7 @@ window.addresses = [
   {
     name: "MOTION (50+) CA",
     address: "6144 Industrial Way. Suite B. Livermore, CA. 94551",
+    coords: [37.7038, -121.7680],
     contacts: [
       "Junior Bandeira (773-501-5285)"
     ],
@@ -66,6 +73,7 @@ window.addresses = [
   {
     name: "PRIME, VITALITY, ENVISION (85-90) IL",
     address: "4300 Commerce Ct, unit 240 Lisle, IL 60532",
+    coords: [41.8023, -88.0884],
     contacts: [
       "VITALITY: Chayse Kidd (208) 360-3024",
       "ENVISION: Rocky Niger (727) 748-6089"
